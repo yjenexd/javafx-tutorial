fa50f17419999a3bfbc0e5d097edb9e820ba96e4
Koh Yu Jun <yjenexd@gmail.com>
2026-09-20 01:32:30 +0800

    Using and styling controls

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,82 @@
+import DialogBox.DialogBox; //the root for java project is src/main/java, so the package name is DialogBox, and the class name is DialogBox. Therefore, the import statement is import DialogBox.DialogBox
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Main extends Application { //main needs to extend Application to be able to run JavaFX applications
+
+public class Main extends Application {
+
+    private ScrollPane scrollPane;
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private Scene scene;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png")); //reference from main
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+
 
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
 
-        stage.setScene(scene); // Setting the stage to show our scene
-        stage.show(); // Render the stage.
+         //Setting up required components
+
+         scrollPane = new ScrollPane(); 
+         dialogContainer = new VBox();
+         scrollPane.setContent(dialogContainer); //fill the box excluding the scroll part
+
+         userInput = new TextField();
+         sendButton = new Button("Send");
+
+         DialogBox dialogBox = new DialogBox("Hello", userImage); //create a dialog box with the text and image
+         dialogContainer.getChildren().add(dialogBox); //add the dialog box to the dialog container
+
+         AnchorPane mainLayout = new AnchorPane();
+         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton); //get the children list of anchor pane and add the control to it
+
+         scene = new Scene(mainLayout);
+
+         stage.setScene(scene);
+         stage.show();
+
+         //Formatting the window to look as expected
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        //More code to be added here later
     }
 }
